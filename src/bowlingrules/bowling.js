28ef16a0b1fa5fc398e30afca442466f886fb4e1
samuishark <gary.norman.th@gmail.com>
2019-08-07 16:23:32 +0700

    added array

--- src/bowlingrules/bowling.js
+++ src/bowlingrules/bowling.js
@@ -1,11 +1,14 @@
 'use strict'
 
-function calculateScore(){
+const frame = [];
+frame.length = 21;
+
+/*function calculateScore(){
     var throw1 = 0;
     var throw2 = 0;
     return throw1 + throw2;
 
-}
+}*/
 
 module.exports = {
     calculateScore
