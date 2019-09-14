521407d6289f60298d3402176a79c94533a044f0
samuishark <gary.norman.th@gmail.com>
2019-09-14 19:16:39 +0700

    cleaned up code

--- src/bowlingrules/bowling.js
+++ src/bowlingrules/bowling.js
@@ -1,9 +1,11 @@
 'use strict'
 
 function calculateScore(throws){
-    var throw1 = 5;
-    var throw2 = 4;
-    return throw1 + throw2;
+    var total = 0;
+    for (var i=0; i<throws.length; i++) {
+        total += throws[i];
+    }
+    return total;
 }
 
 module.exports.calculateScore = calculateScore;
