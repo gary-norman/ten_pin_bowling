d5920f16302c67758959bebc1ecaef976d207b59
Gary <gary@iMacA11.local>
2019-09-18 11:38:26 +0700

    all conceivable final frames pass

--- src/bowlingrules/bowling.js
+++ src/bowlingrules/bowling.js
@@ -2,19 +2,26 @@
 
 function calculateScore(throws){
     var total = 0, j = throws.length;
-    for (var i=0; i<(throws.length-2); i++) {
-        if (throws[j-2] + throws[j-1] < 10) {
-            throws[j] == 0
+    for (var i=0; i<(j-1); i++) {
+        /*if (i === 18 && throws[i] < 10) {
+                throws[i+2] == 0;
+            }
+        else */if (i === 18 && throws[i] === 10)  {
+            throws[i] += throws[i+1] += throws[i+2];
+            throws[i+1] = 0;
         }
         else if (throws[i] === 10 && throws[i+2] === 10) {
             throws[i] += throws[i+2] + throws[i+4];
         }
         else if (throws[i] === 10)   {
-            throws[i+1] += throws[i+2] + throws[i+3];
+            throws[i] += throws[i+2] + throws[i+3];
+            
         }
-        else if (throws[i]<10 && throws[i] + throws[i+1] === 10) {
-            throws[i+1] += throws[i+2];
-        } else {
+        else if (throws[i] < 10 && throws[i+1] < 10 && throws[i] + throws[i+1] === 10) {
+            throws[i] += throws[i+1] + throws[i+2];
+            throws[i+1] = 0;
+        }
+        else {
             throws[i+1] == throws[i+1];
         }
         total += throws[i];
--- test/test-forZero.js
+++ test/test-forZero.js
@@ -26,13 +26,13 @@ let throws = Array(21).fill(0);
 
 describe('Check zero score is zero.', function () {
     it('Test for zero value', function() {
-        assert(bowling.calculateScore(throws) ==  0, "The actual value is "+bowling.calculateScore(throws));
+        assert(bowling.calculateScore(throws) ==  0, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
     })
 
     it('Check 2 throws equal 9', function() {
         throws[0] = 5
         throws[1] = 4
-        assert(bowling.calculateScore(throws) == 9, "The actual value is "+bowling.calculateScore(throws));
+        assert(bowling.calculateScore(throws) == 9, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
     })
 
     it('Check 2 frame spare', function() {
@@ -41,7 +41,7 @@ describe('Check zero score is zero.', function () {
         throws[2] = 4
         throws[3] = 5
 
-        assert(bowling.calculateScore(throws) == 23, "The actual value is "+bowling.calculateScore(throws));
+        assert(bowling.calculateScore(throws) == 23, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
     })
 
     it('check 2 frame strike', function()   {
@@ -50,7 +50,7 @@ describe('Check zero score is zero.', function () {
         throws[2] = 4
         throws[3] = 5
 
-        assert(bowling.calculateScore(throws) == 28, "The actual value is "+bowling.calculateScore(throws));
+        assert(bowling.calculateScore(throws) == 28, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
     })
 
     it('check 3 frame strike + spare', function()   {
@@ -61,7 +61,7 @@ describe('Check zero score is zero.', function () {
         throws[4] = 4
         throws[5] = 5
 
-        assert(bowling.calculateScore(throws) == 48, "The actual value is "+bowling.calculateScore(throws));
+        assert(bowling.calculateScore(throws) == 43, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
     })
 
     it('check perfect game', function()   {
@@ -97,6 +97,114 @@ describe('Check zero score is zero.', function () {
 
         throws[20] = 10
 
-        assert(bowling.calculateScore(throws) == 300, "The actual value is "+bowling.calculateScore(throws));
+        assert(bowling.calculateScore(throws) == 300, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
+    })
+
+    it('check 9 strikes and a spare', function()   {
+        throws[0] = 10
+        throws[1] = 0
+
+        throws[2] = 10
+        throws[3] = 0
+
+        throws[4] = 10
+        throws[5] = 0
+
+        throws[6] = 10
+        throws[7] = 0
+
+        throws[8] = 10
+        throws[9] = 0
+
+        throws[10] = 10
+        throws[11] = 0
+
+        throws[12] = 10
+        throws[13] = 0
+
+        throws[14] = 10
+        throws[15] = 0
+
+        throws[16] = 10
+        throws[17] = 0
+
+        throws[18] = 5
+        throws[19] = 5
+
+        throws[20] = 5
+
+        assert(bowling.calculateScore(throws) == 270, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
+    })
+
+    it('check 9 spares', function()   {
+        throws[0] = 5
+        throws[1] = 5
+
+        throws[2] = 5
+        throws[3] = 5
+
+        throws[4] = 5
+        throws[5] = 5
+
+        throws[6] = 5
+        throws[7] = 5
+
+        throws[8] = 5
+        throws[9] = 5
+
+        throws[10] = 5
+        throws[11] = 5
+
+        throws[12] = 5
+        throws[13] = 5
+
+        throws[14] = 5
+        throws[15] = 5
+
+        throws[16] = 5
+        throws[17] = 5
+
+        throws[18] = 5
+        throws[19] = 5
+
+        throws[20] = 5
+
+        assert(bowling.calculateScore(throws) == 150, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
+    })
+
+    it('check every frame is 9', function()   {
+        throws[0] = 4
+        throws[1] = 5
+
+        throws[2] = 4
+        throws[3] = 5
+
+        throws[4] = 4
+        throws[5] = 5
+
+        throws[6] = 4
+        throws[7] = 5
+
+        throws[8] = 4
+        throws[9] = 5
+
+        throws[10] = 4
+        throws[11] = 5
+
+        throws[12] = 4
+        throws[13] = 5
+
+        throws[14] = 4
+        throws[15] = 5
+
+        throws[16] = 4
+        throws[17] = 5
+
+        throws[18] = 4
+        throws[19] = 5
+
+        throws[20] = 0
+
+        assert(bowling.calculateScore(throws) == 90, "The actual value is " + bowling.calculateScore(throws) + " array = "+throws);
     })
 })
