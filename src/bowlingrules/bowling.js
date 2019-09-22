523edf737fd228cdbf65a867dc3f8a97f59964ec
samuishark <gary.norman.th@gmail.com>
2019-09-22 14:01:40 +0700

    removed switch; all is now functions

--- src/bowlingrules/bowling.js
+++ src/bowlingrules/bowling.js
@@ -1,7 +1,7 @@
 'use strict'
 
 
-/*
+/* if/else
 function calculateScore(throws){
     var total = 0, j = throws.length;
     for (var i=0; i<(j-1); i++) {
@@ -26,6 +26,7 @@ function calculateScore(throws){
 }
 */
 
+/* switch + functions
 function calculateScore(throws){
     var total = 0, j = throws.length;
 
@@ -60,6 +61,41 @@ function calculateScore(throws){
         }
 
 
+        total += throws[i];
+    }
+    return total;
+}
+*/
+
+function calculateScore(throws){
+    var total = 0, j = throws.length;
+
+    function isFinalFrameStrike() {
+        return (i === 18 && throws[i] === 10 ? finalFrameStrike() : isDouble());
+    }
+    function isDouble()    {
+        return (throws[i] === 10 && throws[i+2] === 10? double() : isStrike());
+    }
+    function isStrike()    {
+        return (throws[i] === 10 ? strike(): isSpare());
+    }
+    function isSpare()     {
+        return (throws[i] < 10 && throws[i+1] < 10 && throws[i] + throws[i+1] === 10 ? spare() : throws[i+1] == throws[i+1]);
+    }
+    function finalFrameStrike() {
+        return throws[i] += throws[i+1] += throws[i+2],throws[i+1] = 0;
+    }
+    function double()   {
+        return throws[i] += throws[i+2] + throws[i+4];
+    }
+    function strike()   {
+        return throws[i] += throws[i+2] + throws[i+3];
+    }
+    function spare()    {
+        return throws[i] += throws[i+1] + throws[i+2],throws[i+1] = 0;
+    }
+    for (var i=0; i<(j-1); i++) {
+        isFinalFrameStrike(i);
         total += throws[i];
     }
     return total;
